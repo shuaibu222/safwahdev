0a063d4c1e2e5216e38c466a3abd253a02050eaf
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-07-20 17:30:55 +0100

    first commit in sha Allah

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import page from './sass/page.module.scss'
+
+export default function Home() {
+  return (
+    <main className={page.main}>
+      <h1 className={page.title}>Welcome to Safwahdev coding school!</h1>
+    </main>
+  )
+}

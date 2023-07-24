979cc9efb1d3f4a4884efec9d66c1acc7c7485bd
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-07-24 20:48:32 +0100

    finishing navigation

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -5,12 +5,19 @@ import { Poppins } from 'next/font/google';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 import { HiSun, HiMoon } from 'react-icons/hi';
+import { IoIosMenu } from 'react-icons/io';
+import { LiaTimesSolid } from 'react-icons/lia';
 
 const inter = Poppins({ subsets: ['devanagari'], weight: '700' });
 
 export default function Nav() {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleNavOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     setMounted(true);
@@ -20,43 +27,105 @@ export default function Nav() {
   const currentTheme = theme === 'system' ? systemTheme : theme;
 
   return (
-    <header className='container mx-auto my-0 flex justify-between py-7'>
-      <Link
-        href='/'
-        className={`bg-gradient-to-r from-pink-600 to-yellow-400 bg-clip-text text-2xl text-transparent ${inter.className}`}
+    <header>
+      <div className='container mx-auto my-0 flex items-center justify-between py-7'>
+        <Link
+          href='/'
+          className={`bg-gradient-to-r from-pink-600 to-yellow-400 bg-clip-text text-2xl text-transparent ${inter.className}`}
+        >
+          safwahdev.
+        </Link>
+        <nav className='hidden md:block'>
+          <ul className='flex items-center gap-6'>
+            <li className='border-b-2 border-b-transparent px-1 py-3 hover:border-orange-500 hover:text-orange-500 hover:transition-all hover:duration-150 hover:ease-in-out dark:text-orange-50 dark:hover:text-orange-500'>
+              <Link href='paths'>About</Link>
+            </li>
+            <li className='border-b-2 border-b-transparent px-1 py-3 hover:border-orange-500 hover:text-orange-500 hover:transition-all hover:duration-150 hover:ease-in-out dark:text-orange-50 dark:hover:text-orange-500'>
+              <Link href='paths'>Syllabus</Link>
+            </li>
+            <li className='border-b-2 border-b-transparent px-1 py-3 hover:border-orange-500 hover:text-orange-500 hover:transition-all hover:duration-150 hover:ease-in-out dark:text-orange-50 dark:hover:text-orange-500'>
+              <Link href='bootcamp'>Bootcamps</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className='hidden md:block'>
+          <div className='flex items-center gap-2 dark:text-orange-50'>
+            <div className='flex items-center justify-center'>
+              {currentTheme === 'dark' ? (
+                <HiSun
+                  onClick={() => setTheme('light')}
+                  className=' text-2xl'
+                />
+              ) : (
+                <HiMoon
+                  onClick={() => setTheme('dark')}
+                  className=' text-2xl'
+                />
+              )}
+            </div>
+            <Link href='#' className='px-3 py-2'>
+              Sign in
+            </Link>
+            <Link
+              href='#'
+              className='rounded-lg bg-orange-500 bg-gradient-to-r px-3 py-2'
+            >
+              Sign up
+            </Link>
+          </div>
+        </div>
+        <div
+          className='grid place-content-center text-3xl md:hidden'
+          onClick={handleNavOpen}
+        >
+          {isOpen ? <LiaTimesSolid /> : <IoIosMenu />}
+        </div>
+      </div>
+      <div
+        className={
+          isOpen
+            ? 'w-full rounded-md bg-gradient-to-tr from-pink-600 to-yellow-400 p-5 text-white transition duration-100 md:hidden'
+            : 'hidden w-full rounded-md bg-gradient-to-tr from-pink-600 to-yellow-400 p-5 text-white transition duration-100 md:hidden'
+        }
       >
-        safwahdev.
-      </Link>
-      <nav className='flex items-center justify-center'>
-        <ul className='flex items-center gap-6'>
-          <li className='border-b-2 border-b-transparent px-1 py-3 hover:border-orange-500 hover:text-orange-500 hover:transition-all hover:duration-150 hover:ease-in-out dark:text-orange-50 dark:hover:text-orange-500'>
+        <ul className='flex flex-col gap-2'>
+          <li className='border-b-2 border-b-transparent px-1 py-3 text-center hover:rounded hover:bg-black hover:text-white  hover:transition-all hover:duration-150 hover:ease-in-out'>
             <Link href='paths'>About</Link>
           </li>
-          <li className='border-b-2 border-b-transparent px-1 py-3 hover:border-orange-500 hover:text-orange-500 hover:transition-all hover:duration-150 hover:ease-in-out dark:text-orange-50 dark:hover:text-orange-500'>
+          <li className='border-b-2 border-b-transparent px-1 py-3 text-center hover:rounded hover:bg-black hover:text-white  hover:transition-all hover:duration-150 hover:ease-in-out'>
             <Link href='paths'>Syllabus</Link>
           </li>
-          <li className='border-b-2 border-b-transparent px-1 py-3 hover:border-orange-500 hover:text-orange-500 hover:transition-all hover:duration-150 hover:ease-in-out dark:text-orange-50 dark:hover:text-orange-500'>
+          <li className='border-b-2 border-b-transparent px-1 py-3 text-center hover:rounded hover:bg-black hover:text-white  hover:transition-all hover:duration-150 hover:ease-in-out'>
             <Link href='bootcamp'>Bootcamps</Link>
           </li>
         </ul>
-      </nav>
-      <div className='flex items-center gap-2 dark:text-orange-50'>
-        <div className='flex items-center justify-center'>
-          {currentTheme === 'dark' ? (
-            <HiSun onClick={() => setTheme('light')} className=' text-2xl' />
-          ) : (
-            <HiMoon onClick={() => setTheme('dark')} className=' text-2xl' />
-          )}
+        <div className='mt-6 grid items-center gap-3 dark:text-orange-50'>
+          <div className='flex cursor-pointer items-center justify-center'>
+            {currentTheme === 'dark' ? (
+              <HiSun
+                onClick={() => setTheme('light')}
+                className=' cursor-pointer text-2xl'
+              />
+            ) : (
+              <HiMoon
+                onClick={() => setTheme('dark')}
+                className=' cursor-pointer text-2xl'
+              />
+            )}
+          </div>
+          <Link
+            href='#'
+            className='px-3 py-3 text-center hover:rounded hover:bg-orange-500 dark:hover:bg-black'
+          >
+            Sign in
+          </Link>
+          <Link
+            href='#'
+            className='px-3 py-3 text-center text-orange-50 hover:rounded hover:bg-orange-500 dark:hover:bg-black'
+          >
+            Sign up
+          </Link>
         </div>
-        <Link href='#' className='px-3 py-2'>
-          Sign in
-        </Link>
-        <Link
-          href='#'
-          className='rounded-lg bg-gradient-to-r from-pink-600 to-yellow-400 px-3 py-3'
-        >
-          Get Started
-        </Link>
       </div>
     </header>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import { Poppins } from 'next/font/google';
 import Nav from './components/Nav';
 import Link from 'next/link';
+import { LiaArrowDownSolid } from 'react-icons/lia';
 
 const inter = Poppins({ subsets: ['devanagari'], weight: '600' });
 
@@ -8,12 +9,12 @@ export default function Home() {
   return (
     <main className='mx-auto my-0 min-h-screen w-11/12 sm:max-w-7xl'>
       <Nav />
-      <section className='mt-28 flex flex-col'>
+      <section className='mt-28 flex h-full flex-col'>
         <h1 className='sm:text-xlg flex flex-col gap-1 text-center text-lg dark:text-white md:text-2xl'>
           Become a
           <br />
           <span
-            className={`bg-gradient-to-r from-pink-600 to-yellow-400 bg-clip-text py-5 text-4xl text-transparent sm:text-6xl md:text-8xl ${inter.className}`}
+            className={`bg-gradient-to-r from-pink-600 to-yellow-400 bg-clip-text py-5 text-5xl text-transparent sm:text-6xl md:text-8xl ${inter.className}`}
           >
             Frontend Engineer
           </span>
@@ -28,9 +29,15 @@ export default function Home() {
         </p>
         <Link
           href='signup'
-          className='hover:delay-50 mt-7 self-center rounded-md border-2 border-black px-5 py-3  text-lg hover:bg-orange-50 hover:transition dark:border-orange-100 dark:hover:bg-orange-100 dark:hover:text-black'
+          className='hover:delay-50 mt-7 self-center rounded-md border-2 border-black px-5 py-3 text-sm  hover:bg-orange-50 hover:transition dark:border-orange-100 dark:hover:bg-orange-100 dark:hover:text-black sm:text-lg'
         >
           Sign up it's free!
+        </Link>
+        <Link
+          href='/'
+          className='mt-24 animate-bounce cursor-pointer self-center justify-self-end rounded-3xl border-2 border-orange-500 p-3 text-2xl'
+        >
+          <LiaArrowDownSolid />
         </Link>
       </section>
     </main>
